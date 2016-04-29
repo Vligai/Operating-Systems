62c8d1c5d0cf0e27cdfe58ca2c394bc9d8b129d4
vligai <vligai@stevens.edu>
2016-04-29 13:12:02 -0400

    added tree from http://tree.phi-sci.com/

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -1,7 +1,27 @@
 #include "assignment3.h"
+tree<tree_node> filesystem;
+tree<tree_node>::iterator workingDirectory;
+
+list<block>* disk;
+
+unsigned int counter;
+unsigned int diskSize;
+unsigned int blockSize;
+unsigned int numBlocks;
+
+
 
 int main(int argc, char** argv)
 {
+  if(argc != 8)
+    {
+      printf("-f [input files storing information on files]\n"
+	   "-d [input files storing information on directories]\n"
+	   "-s [disk size]\n"
+	   "-b [block size]\n");
+      exit(1);
+    }
+  
+  
   return 1;
-
 }
